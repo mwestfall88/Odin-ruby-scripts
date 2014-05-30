2f39136fe4f40e5c610f7979bb8ac9675cfe8341
Mark Westfall <marktwestfall@gmail.com>
2014-05-29 23:54:52 -0700

    Included program description in header

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,3 +1,6 @@
+#Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
+
+
 def caesar_cipher(str, i)
 	alphabet = ('a'..'z').to_a
 	cipher  = ""
